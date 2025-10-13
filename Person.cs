dcaa9ed631123177b6d5db284bdb308437606065
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-13 14:21:18 +0200

    Added Person class

---
+++ Person.cs
@@ -0,0 +1,6 @@
+namespace Tjuv_Och_Polis_Group_Project;
+
+public class Person
+{
+    public string Name { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tjuv_Och_Polis_Group_Project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
