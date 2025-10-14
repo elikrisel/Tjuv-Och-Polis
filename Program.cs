69fd42b6c3516f790bf28de95d7b7252b53f8a64
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-14 09:54:17 +0200

    Nytt meddelande i main från Kristofer branch

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
+        Console.WriteLine("Hello from Kristofer ------ branch test");
         
         Console.WriteLine("Hello Third world");
 
