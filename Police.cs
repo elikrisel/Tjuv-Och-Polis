bdc508265f8ed408ab4cbf11d515c0ba73d25eb2
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-14 10:44:20 +0200

    Created a Police class that inherits from Person.cs

---
+++ Police.cs
@@ -0,0 +1,9 @@
+namespace Tjuv_Och_Polis_Group_Project;
+
+class Police : Person
+{
+    public Police(string name, string description, int[,] coordinationSystem, char character) : base(name, description, coordinationSystem, character)
+    {
+        
+    }
+}
