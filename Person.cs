25372995b8dd8b61b7b93a4b666ddb6615dfa976
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-14 10:40:17 +0200

    Implemented variables in Person.cs and generated a new class that inherits Person

--- Person.cs
+++ Person.cs
@@ -1,8 +1,27 @@
+using System.Reflection.PortableExecutable;
+
 namespace Tjuv_Och_Polis_Group_Project;
 
 public class Person
 {
-    public string Name { get; set; }
+    public string Name { get; set; } //NAMN
 
-    public string Description { get; set; }
+    public string Description { get; set; } //BESKRIVNING
+
+    public int[,] CoordinationSystem { get; set; }
+
+    //public bool InAction { get; set; } // OM DE ÄR PÅ SAMMA PLATS?
+
+    public List<string> InventorySystem { get; set; }
+
+    public char Character { get; set; }
+
+    public Person(string name, string description, int[,] coordinationSystem, char character)
+    {
+        Name = name;
+        Description = description;
+        CoordinationSystem = coordinationSystem;
+        Character = character;
+    }
+    
 }
---
+++ Tjuv.cs
@@ -0,0 +1,9 @@
+namespace Tjuv_Och_Polis_Group_Project;
+
+class Tjuv : Person
+{
+    public Tjuv(string name, string description, int[,] coordinationSystem, char character) : base(name, description, coordinationSystem, character)
+    {
+        
+    }
+}
