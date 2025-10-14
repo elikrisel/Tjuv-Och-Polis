f42483fe618068a7a3776d491aa9d3e75e9a6fd1
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-14 10:55:48 +0200

    Created a Prison that inherits from Place.cs

---
+++ Prison.cs
@@ -0,0 +1,9 @@
+namespace Tjuv_Och_Polis_Group_Project;
+
+class Prison : Place
+{
+    public Prison(int rows, int columns, string[,] mapGrid, char border) : base(rows, columns, mapGrid, border)
+    {
+        
+    }
+}
