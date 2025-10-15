d14976f48b1084b92827e8928db206db5689c4da
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-15 15:17:09 +0200

    Commented Testing: WASD in region and uncommented Console.ReadKey at the bottom of the program

--- Program.cs
+++ Program.cs
@@ -92,39 +92,39 @@ class Program
             //Thread.Sleep(test);
             #endregion
             #region TESTING : WASD movement for debug
-            ConsoleKeyInfo key = Console.ReadKey(true);
-
-            switch (key.Key)
-            {
-                case ConsoleKey.W: posX--; if (posX == 0) posX = cityRows - 2; break; // !inJail på alla ifsatser
-                case ConsoleKey.S: posX++; if (posX == cityRows - 1) posX = 1; break;
-                case ConsoleKey.A: posY--; if (posY == 0) posY = cityCols - 2; break;
-                case ConsoleKey.D: posY++; if (posY == cityCols - 1) posY = 1; break;
-
-                case ConsoleKey.J: inJail = !inJail; Console.Clear(); break;
-                case ConsoleKey.L: debugList = !debugList; Console.Clear(); break;
-                case ConsoleKey.R:
-                    {
-                        switch (Random.Shared.Next(0, 9))
-                        {
-                            case 0: posX--; if (posX == 0) posX = cityRows - 2; break;
-                            case 1: posX++; if (posX == cityRows - 1) posX = 1; break;
-                            case 2: posY--; if (posY == 0) posY = cityCols - 2; break;
-                            case 3: posY++; if (posY == cityCols - 1) posY = 1; break;
-                            case 4: posX--; if (posX == 0) posX = cityRows - 2; posY--; if (posY == 0) posY = cityCols - 2; break;
-                            case 5: posX--; if (posX == 0) posX = cityRows - 2; posY++; if (posY == cityCols - 1) posY = 1; break;
-                            case 6: posX++; if (posX == cityRows - 1) posX = 1; posY--; if (posY == 0) posY = cityCols - 2; break;
-                            case 7: posX++; if (posX == cityRows - 1) posX = 1; posY++; if (posY == cityCols - 1) posY = 1; break;
-                            case 8: Console.WriteLine("Didnt move"); break;
-                        }
-                        break;
-                    }
-            }
+            // ConsoleKeyInfo key = Console.ReadKey(true);
+            //
+            // switch (key.Key)
+            // {
+            //     case ConsoleKey.W: posX--; if (posX == 0) posX = cityRows - 2; break; // !inJail på alla ifsatser
+            //     case ConsoleKey.S: posX++; if (posX == cityRows - 1) posX = 1; break;
+            //     case ConsoleKey.A: posY--; if (posY == 0) posY = cityCols - 2; break;
+            //     case ConsoleKey.D: posY++; if (posY == cityCols - 1) posY = 1; break;
+            //
+            //     case ConsoleKey.J: inJail = !inJail; Console.Clear(); break;
+            //     case ConsoleKey.L: debugList = !debugList; Console.Clear(); break;
+            //     case ConsoleKey.R:
+            //         {
+            //             switch (Random.Shared.Next(0, 9))
+            //             {
+            //                 case 0: posX--; if (posX == 0) posX = cityRows - 2; break;
+            //                 case 1: posX++; if (posX == cityRows - 1) posX = 1; break;
+            //                 case 2: posY--; if (posY == 0) posY = cityCols - 2; break;
+            //                 case 3: posY++; if (posY == cityCols - 1) posY = 1; break;
+            //                 case 4: posX--; if (posX == 0) posX = cityRows - 2; posY--; if (posY == 0) posY = cityCols - 2; break;
+            //                 case 5: posX--; if (posX == 0) posX = cityRows - 2; posY++; if (posY == cityCols - 1) posY = 1; break;
+            //                 case 6: posX++; if (posX == cityRows - 1) posX = 1; posY--; if (posY == 0) posY = cityCols - 2; break;
+            //                 case 7: posX++; if (posX == cityRows - 1) posX = 1; posY++; if (posY == cityCols - 1) posY = 1; break;
+            //                 case 8: Console.WriteLine("Didnt move"); break;
+            //             }
+            //             break;
+            //         }
+            // }
             #endregion
             
             citizen.Move(city);
-            //Console.ReadKey(true);
-
+            Console.ReadKey(true);
+            
         }
     }
 }
