810e6cae9e6c848485bddc8e3d3317ee422e6d30
Kristofer Sell <kristoferelissell@gmail.com>
2025-10-21 09:41:56 +0200

    Added interaction between citizen and police and refactoring so all the people have their interaction own interaction methods

--- PersonManager.cs
+++ PersonManager.cs
@@ -28,45 +28,10 @@ public class PersonManager
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"{persons[i].Description} {persons[i].Name} and {persons[j].Description} {persons[j].Name} are at same place");
-                    if (person1 is Citizen && person2 is Thief)
-                    {
-                        // Console.WriteLine("person1 medborgare stöter på person2 tjuv");
-                        if (person1.InventorySystem.Count > 0)
-                        {
-                            person2.InventorySystem.Add(person1.InventorySystem[0]);
-                            person1.InventorySystem.RemoveAt(0);    
-                        }  
-                        
-                    }
-                    else if (person1 is Thief && person2 is Citizen)
-                    {
-                        // Console.WriteLine("person1 tjuv stöter på person2 medborgare");
-                        if (person2.InventorySystem.Count > 0)
-                        {
-                            person1.InventorySystem.Add(person2.InventorySystem[0]);
-                            person2.InventorySystem.RemoveAt(0);
-                        }
-                        
-                    }
-                    else if(person1 is Police && person2 is Thief)
-                    {
-                        if (person2.InventorySystem.Count > 0)
-                        {
-                            person1.InventorySystem.AddRange(person2.InventorySystem);
-                            person2.InventorySystem.Clear();
-                            ((Thief)person2).MoveToJail(person2,prison);
-                            
-                        }
-                    }
-                    else if (person1 is Thief && person2 is Police)
-                    {
-                        if (person1.InventorySystem.Count > 0)
-                        {
-                            person2.InventorySystem.AddRange(person1.InventorySystem);
-                            person1.InventorySystem.Clear();
-                            ((Thief)person1).MoveToJail(person1,prison);
-                        }
-                    }
+                    HandleCitizenAndThiefInteraction(person1, person2);
+                    HandleThiefAndPoliceInteraction(person1, person2,prison);
+                    HandleCitizenAndPoliceInteraction(person1, person2);
+                   
                     
                 }
                 // else
@@ -79,8 +44,71 @@ public class PersonManager
             Console.ResetColor();
         }
         
+        
 
+        
+    }
 
+    private static void HandleCitizenAndPoliceInteraction(Person person1, Person person2)
+    {
+        if (person1 is Citizen && person2 is Police)
+        {
+            Console.WriteLine($"{person1.Description} {person1.Name} hälsar på: {person2.Description} {person2.Name}");
+        }
+        else if (person1 is Police && person2 is Citizen)
+        {
+            Console.WriteLine($"{person2.Description} {person2.Name} hälsar på: {person1.Description} {person1.Name}");
+        }
+    }
 
+    private static void HandleThiefAndPoliceInteraction(Person person1, Person person2, Prison prison)
+    {
+        if(person1 is Police && person2 is Thief)
+        {
+            if (person2.InventorySystem.Count > 0)
+            {
+                person1.InventorySystem.AddRange(person2.InventorySystem);
+                person2.InventorySystem.Clear();
+                ((Thief)person2).MoveToJail(person2,prison);
+                Console.WriteLine("Thief is getting send to prison!");
+                            
+            }
+        }
+        else if (person1 is Thief && person2 is Police)
+        {
+            if (person1.InventorySystem.Count > 0)
+            {
+                person2.InventorySystem.AddRange(person1.InventorySystem);
+                person1.InventorySystem.Clear();
+                ((Thief)person1).MoveToJail(person1,prison);
+                Console.WriteLine("Thief is getting send to prison!"); 
+            }
+        }
+    }
+
+    private static void HandleCitizenAndThiefInteraction(Person person1, Person person2)
+    {
+        if (person1 is Citizen && person2 is Thief)
+        {
+            // Console.WriteLine("person1 medborgare stöter på person2 tjuv");
+            if (person1.InventorySystem.Count > 0)
+            {
+                person2.InventorySystem.Add(person1.InventorySystem[0]);
+                person1.InventorySystem.RemoveAt(0);
+                Console.WriteLine("Citizen losing an item");
+            }  
+                        
+        }
+        else if (person1 is Thief && person2 is Citizen)
+        {
+            // Console.WriteLine("person1 tjuv stöter på person2 medborgare");
+            if (person2.InventorySystem.Count > 0)
+            {
+                person1.InventorySystem.Add(person2.InventorySystem[0]);
+                person2.InventorySystem.RemoveAt(0);
+                Console.WriteLine("Citizen losing an item");
+            }
+                        
+        }
     }
 }
